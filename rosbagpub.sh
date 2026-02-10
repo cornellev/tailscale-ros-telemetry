2a15fd25263a0b3d6e919b9593d39ef01ea065d0
shreyaa <shreyaa.sanjay@gmail.com>
2026-02-10 18:32:34 -0500

    add rosbag volume to docker compose

--- rosbagpub.sh
+++ rosbagpub.sh
@@ -3,13 +3,13 @@
 set -e
 
 TOPIC_NAME="spi_data"
-BAG_NAME="publisher_data_bag"
+BAG_NAME="/worksapce/bags/publisher_data_bag"
 #PUBLISHER_CMD="ros2 run talker"
 
 sleep 2
 
-echo "Recording topic $TOPIC_NAME to rosbag..."
-exec ros2 bag record $TOPIC_NAME -o $BAG_NAME 
+echo "Recording topic '$TOPIC_NAME' to rosbag in directory '$BAG_NAME'..."
+exec ros2 bag record "$TOPIC_NAME" -o "$BAG_NAME" 
 
 
 
