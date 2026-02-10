82f3770740b307bc6896e7d70bc3e7e2a8a678e6
shreyaa <shreyaa.sanjay@gmail.com>
2026-02-10 18:07:04 -0500

    -update rosbag shell

--- rosbagpub.sh
+++ rosbagpub.sh
@@ -4,18 +4,12 @@ set -e
 
 TOPIC_NAME="spi_data"
 BAG_NAME="publisher_data_bag"
-PUBLISHER_CMD="ros2 run talker"
-
-echo "Starting publisher..."
-$PUBLISHER_CMD &
-PUBLISHER_PID=$!
+#PUBLISHER_CMD="ros2 run talker"
 
 sleep 2
 
 echo "Recording topic $TOPIC_NAME to rosbag..."
-ros2 bag record $TOPIC_NAME -o $BAG_NAME &
-BAG_PID=$!
+exec ros2 bag record $TOPIC_NAME -o $BAG_NAME 
 
-trap "echo 'Stopping...'; kill $BAG_PID $PUBLISHER_PID; exit" SIGINT
 
-wait
+
