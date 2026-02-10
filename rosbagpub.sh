f6fd9d348c1b0a14a0ab4687dd2db69d6cf6a18c
shreyaa <shreyaa.sanjay@gmail.com>
2026-02-10 18:47:57 -0500

    silent record

--- rosbagpub.sh
+++ rosbagpub.sh
@@ -9,7 +9,7 @@ BAG_NAME="/worksapce/bags/publisher_data_bag"
 sleep 2
 
 echo "Recording topic '$TOPIC_NAME' to rosbag in directory '$BAG_NAME'..."
-exec ros2 bag record "$TOPIC_NAME" -o "$BAG_NAME" 
+exec ros2 bag record "$TOPIC_NAME" -o "$BAG_NAME"  >/dev/null 2>&1
 
 
 
