2797875f47d9433143c3e7939dbca25c471559b7
erics118 <ericshen118@gmail.com>
2026-03-03 22:42:42 -0500

    remove rmw_implementation

--- entrypoint-rosbag.sh
+++ entrypoint-rosbag.sh
@@ -4,7 +4,6 @@ set -e
 source /opt/ros/humble/setup.bash
 source /ros-telemetry/install/setup.bash
 
-echo "RMW_IMPLEMENTATION is set to: ${RMW_IMPLEMENTATION:-<unset>}"
 echo "ROS_DOMAIN_ID is set to: ${ROS_DOMAIN_ID:-<unset>}"
 
 # ensure ros logs show up in container logs reliably
