5f2118207c8a7e7743678f60994cafece46d59aa
erics118 <52634785+erics118@users.noreply.github.com>
2026-03-10 21:36:00 -0400

    daniel pls

---
+++ docker/entrypoint-subscriber.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e
+
+source /opt/ros/humble/setup.bash
+source /ros-telemetry/install/setup.bash
+
+echo "ROS_DOMAIN_ID is set to: ${ROS_DOMAIN_ID:-<unset>}"
+
+# ensure ros logs show up in container logs reliably
+export RCUTILS_LOGGING_USE_STDOUT=1
+export RCUTILS_LOGGING_BUFFERED_STREAM=0
+export PYTHONUNBUFFERED=1
+
+echo "Starting listener node..."
+exec ros2 run py_pubsub listener
